a50644b81a90fc7688b3b3d544daaf2312293072
Slava <slavasotnikov2020@gmail.com>
2021-04-19 16:19:00 +0200

    Add methods in BinarySearch

--- BinarySearch/Program.cs
+++ BinarySearch/Program.cs
@@ -4,9 +4,11 @@ namespace BinarySearch
 {
     class Program
     {
+        static void Show() { Console.WriteLine("Number doesn't exist."); }
+        static void ShowIndex(int a, int b) { Console.WriteLine("Number {0} array's index {1}", a, b); }
         static void Main()
         {
-            int x = 64;
+            int x = 67;
             int[] myArr = { -55, -49, -17, 8, 41, 56, 60, 67, 85, 99 };
 
             int left = 0;
@@ -18,45 +20,42 @@ namespace BinarySearch
             {
                 if (x == myArr[z])
                 {
-                    Console.WriteLine("Number {0} array's index {1}", x, z);
-                    break;
+                    ShowIndex(x, z); break;
                 }
+
                 if (x > myArr[z])
                 {
                     left = ++z;
                     if (left >= myArr.Length)
                     {
-                        Console.WriteLine("Number doesn't exist.");
-                        break;
+                        Show(); break;
                     }
                     if (x == myArr[left])
                     {
-                        Console.WriteLine("Number {0} array's index {1}", x, left);
-                        break;
+                        ShowIndex(x, left); break;
                     }
                     z = (left + right) >> 1;
                 }
+
                 if (x < myArr[z])
                 {
                     right = --z;
                     if (0 > right || right < left)
                     {
-                        Console.WriteLine("Number doesn't exist.");
-                        break;
+                        Show(); break;
                     }
                     if (x == myArr[right])
                     {
-                        Console.WriteLine("Number {0} array's index {1}", x, right);
-                        break;
+                        ShowIndex(x, right); break;
                     }
                     z = (left + right) >> 1;
                 }
 
                 if (left == right)
                 {
-                    Console.WriteLine("Number doesn't exist.");
-                    break;
+                    Show(); break;
                 }
+
             } while (left != right);
 
             Console.ReadKey();
