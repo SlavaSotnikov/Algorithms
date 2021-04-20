602c060867989f25938b02f0995af8e47db8864e
Slava <slavasotnikov2020@gmail.com>
2021-04-20 16:36:05 +0200

    Insertion Sort

---
+++ InsertionSort/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace InsertionSort
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] yourArr = { 66, -62, -28, -26, -3, 75, 89, -85, 23, -48 };
+
+            int i = 0;
+            int memory = 0;
+
+            Console.Write("Your array: ");
+
+            for (int k = 0; k < yourArr.Length; k++)
+            {
+                Console.Write("{0} ", yourArr[k]);
+            }
+
+            while (i < yourArr.Length - 1)
+            {
+                if (yourArr[i] > yourArr[i + 1])
+                {
+                    (yourArr[i], yourArr[i + 1]) = (yourArr[i + 1], yourArr[i]);
+                    if (i != 0)
+                    {
+                        i--;
+                    }
+                    else
+                    {
+                        memory++;
+                        i = memory;
+                    }
+                }
+                else
+                {
+                    memory++;
+                    i = memory;
+                }
+            }
+
+            Console.WriteLine();
+
+            Console.Write("Sort array: ");
+
+            for (int l = 0; l < yourArr.Length; l++)
+            {
+                Console.Write("{0} ", yourArr[l]);
+            }
+
+            Console.ReadKey();                        
+        }
+    }
+}
